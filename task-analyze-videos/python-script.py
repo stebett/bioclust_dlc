4b67fb4b99d51839e63048673fd2b9fc2df84ec5
Stefano Bettani <s.bettani@tuta.io>
2024-01-26 10:40:00 +0100

    added dynamic cropping to analyze videos

--- task-analyze-videos/python-script.py
+++ task-analyze-videos/python-script.py
@@ -5,6 +5,6 @@ config_path = "automatically filled, do not change"
 
 videos = str(Path(config_path).parent / "videos")
 
-deeplabcut.analyze_videos(config_path, videos, save_as_csv=True)
+deeplabcut.analyze_videos(config_path, videos, dynamic=(True,.5,10), save_as_csv=True)
 deeplabcut.filterpredictions(config_path, videos)
 deeplabcut.plot_trajectories(config_path, videos)
